3f792ed32c89e8190ffe078452638ba40ddf4f59
ale1201 <ma.pabon@uniandes.edu.co>
2021-10-31 22:45:12 -0500

    arreglos

--- prediction.py
+++ prediction.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from tensorflow import keras
 from keras.preprocessing.image import load_img, img_to_array
 from keras.models import load_model
 modelo = './modelo.h5'
@@ -9,22 +10,35 @@ cnn.load_weights(pesos_modelo)
 def predict(file):
   x = load_img(file, target_size=(180, 180))
   x = img_to_array(x)
+import cv2
+import numpy as np
+from tensorflow import keras
+from keras.preprocessing.image import load_img, img_to_array
+from keras.models import load_model
+import matplotlib.pyplot as plt
+modelo = './modelo/modelo.h5'
+pesos_modelo = './modelo/pesos.h5'
+cnn = load_model(modelo)
+cnn.load_weights(pesos_modelo)
+def predict(file):
+  x = cv2.resize(file, dsize=(180, 180))
+  x = img_to_array(x)
   x = np.expand_dims(x, axis=0)
   array = cnn.predict(x)
   result = array[0]
   answer = np.argmax(result)
   if answer == 0:
     answer = 3
-    print("     -     pred: crosswalk (3)")
+    print("pred: crosswalk (3)")
   elif answer == 1:
     answer = 2
-    print("     -     pred: speedlimit (2)")
+    print("pred: speedlimit (2)")
   elif answer == 2:
     answer = 1
-    print("     -     pred: stop (1)")
+    print("pred: stop (1)")
   elif answer == 3:
     answer = 0
-    print("     -     pred: trafficlight (0)")
+    print("pred: trafficlight (0)")
   else:
     print (answer)
 
@@ -39,5 +53,15 @@ if vc.isOpened():
 else:
   is_capturing = False
 
-
-predict(frame)
+while is_capturing:
+  try:
+    is_capturing, frame = vc.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    predict(frame)
+    webcam_preview = plt.imshow(frame)
+    try:
+      plt.pause(5)
+    except Exception:
+      pass
+  except KeyboardInterrupt:
+    vc.release()
